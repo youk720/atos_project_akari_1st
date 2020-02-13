de834f49ba703933f05785319acb85b11686ffa1
youk233_231 <tjm.yk0259@gmail.com>
2020-02-13 15:20:56 +0900

    2020/2/13_2

---
+++ atos_2.js
@@ -0,0 +1,643 @@
+'use strict';
+
+let res_den = "列車";
+$('[name=type_1]').change(function() {
+  if($('[name=type_1][value=0]').prop('checked')){
+    res_den = "列車"
+  }else if($('[name=type_1][value=1]').prop('checked')){
+    res_den = "電車";
+  }
+})
+
+// 接近:放送順序デフォルト定義
+let jun = [
+  ["接近メロディ\n空白0.5秒"],
+  ["まもなく"],
+  [""],
+  [""], //当駅始発 or no
+  [""], //路線愛称
+  [""], //路線
+  [""], //直通他
+  [""], //種別
+  [""], //行先
+  [""],
+  ["行きが"],
+  ["まいります"],
+  ["危ないですから、黄色い点字ブロックまで、お下がりください\n空白0.25秒"],
+  [""], //この列車は
+  [""], //ドア数
+  [""], //両数
+  [""],
+  [""], //グリーン
+  [""], //この列車の
+  [""], //　前後ろ
+  [""], //両数
+  [""], // は
+  [""], //切り離し駅
+  [""], //止まりとなります or 増結をいたします
+  [""]
+]
+let jun_yuru = [
+  ["接近メロディ\n空白0.5秒"],
+  ["まもなく"],
+  [""],
+  [], //種別
+  [], //列車が
+  ["まいります"], //まいります
+  ["危ないですから、黄色い点字ブロックまで、お下がりください\n空白0.25秒"],
+  [""]
+]
+// 折り返し用接近
+let arriv_back = [
+  ["接近メロディ\n空白0.5秒"],
+  ["まもなく"],
+  [""],
+  ["当駅止まりの"],
+  [""],
+  ["危ないですから、黄色い点字ブロックまで、お下がりください\n空白0.25秒"],
+  [""],
+  [""], //ドア数
+  [""], //両数
+  [""],
+  [""], //グリーン
+  ["空白0.5秒\n折り返し"],
+  [""], //路線愛称
+  [""], //路線
+  [""], //直通他
+  [""], //種別
+  [""], //行先
+  [""], //行先2
+  [""],
+  ["行きと"],
+  ["なります"],
+  [""],
+  [""]
+]
+// 戸閉用
+let close_atos = [
+  ["3番線"], //番線
+  [""], //路線愛称
+  [""], //路線
+  [""], //直通他
+  [""], //種別
+  [""], //行先
+  [""], //行先2
+  [""], // 方面
+  [""], //行きが
+  [""], //発車いたします
+  [""],
+  ["ドアが閉まります"],
+  ["ご注意ください"]
+]
+// 予告:順序デフォルト定義
+let info = [
+  ["本日も、ご利用いただき、ありがとうございます"],
+  ["今度の"],
+  [""],
+  [""], // 停車中の
+  [""],
+  [""], //路線愛称
+  [""], //路線
+  [""], //直通他
+  [""], //種別
+  [""], //行先
+  [""],
+  [""], //方面
+  ["行きです"],
+  [""], //この列車は
+  [""], //ドア数
+  [""], //両数
+  [""],
+  [""], // green
+  [""], //この列車の
+  [""], //　前後ろ
+  [""], //両数
+  [""], // は
+  [""], //切り離し駅
+  [""], //止まりとなります or 増結をいたします
+  [""]
+
+]
+
+// 列車データ格納
+let atos_sentence = [
+  [""], //番線
+  [""], //路線愛称
+  [""], //路線
+  [""], //直通他
+  [""], //種別
+  ["池袋"], //行先
+  [""], //行先2
+  [""], //ドア
+  [""], //両数
+  [""], // グリーン
+  [""], //この列車の
+  [""], //　前後ろ
+  [""], //両数
+  [""], // は
+  [""], //切り離し駅
+  [""], //止まりとなります or 増結をいたします
+  [""], //手動入力
+]
+
+// ATOSモード定義, 予告:1, 接近;2, 発車:3
+let atos_mode = 0;
+
+// 番線選択
+let bn_num = 0;
+$("#bansen").change(function(){
+
+  if($("#bansen").val() != 80){
+    bn_num = $("#bansen").val();
+    atos_sentence[0] = bansen_num[bn_num][1];
+  }else{
+    atos_sentence[0] = "";
+  }
+});
+
+// 路線名
+let line_num = 1;
+$("#line").change(function(){
+  line_num = $("#line").val();
+  atos_sentence[2] = train_line[line_num][1];
+});
+
+// 直通/周り追加判定
+$("[name=line_2]").change(function(){
+  // 直通
+  if($('[name=line_2][value=2]').prop('checked')){
+    // 直通パーツ,追加
+    for(d=0;d<train_line.length;d++){
+      if(atos_sentence[2]+"直通" === train_line[d][1]){
+        atos_sentence[2]=atos_sentence[2]+"直通";
+        break;
+      }else if(d === (train_line.length-1) && atos_sentence[2]+"直通" != train_line[d][1]){
+        atos_sentence[3] = "直通";
+      }
+    }
+  // 周り
+  }else if($('[name=line_2][value=3]').prop('checked')){
+    atos_sentence[3] = "周り";
+  // 無し
+  }else if($('[name=line_2][value=4]').prop('checked')){
+    // atos_sentence[2] = "";
+    atos_sentence[2] = train_line[line_num][1];
+    atos_sentence[3] = "";
+    }
+});
+
+// 種別任意選択
+let kind_num = 1;
+$("#kind").change(function(){
+  kind_num = $("#kind").val();
+  atos_sentence[4] = train_type[kind_num][1];
+});
+
+// 行き先任意選択
+let bound = 0;
+$("#destination").change(function(){
+  bound = $("#destination").val();
+  atos_sentence[5] = buund_for[bound][1];
+  
+});
+// 行先2
+let bound_2 = 0;
+$("#destination_2").change(function(){
+  bound_2 = $("#destination_2").val();
+  atos_sentence[6] = buund_for[bound_2][1];
+});
+
+
+
+// 危ないですからry の種類選択
+let seafty = 40;
+$("#seafty").change(function(){
+  seafty = $("#seafty").val();
+  jun[12] = atos_cross[seafty][1]+"\n空白0.25秒";
+  jun_yuru[6] = atos_cross[seafty][1]+"\n空白0.25秒";
+  arriv_back[5] = atos_cross[seafty][1];
+})
+
+//ドア数
+let door_at = 78;
+$("#doors").change(function(){
+  door_at = $("#doors").val();
+  atos_sentence[7] = atos_cross[door_at][1];
+})
+
+// 両数
+let cars_num = 0;
+$("#cars").change(function(){
+  cars_num = $("#cars").val();
+  // atos_sentence[8] = cars[cars_num][1];
+  for(d=1;d<cars.length;d++){
+    if(cars[cars_num][1]+"です" === cars[d][1]){
+      atos_sentence[8] = cars[cars_num][1]+"です";
+      break;
+    }else if(d === (cars.length-1) && cars[cars_num][1]+"です" != cars[d][1]){
+      atos_sentence[8] = cars[cars_num][1]+"\nです";
+    }
+  }
+  
+})
+
+//グリーン車案内 
+$("#green_car").change(function(){
+  if($("#green_car").prop('checked') == true){
+    atos_sentence[9] = "空白0.5秒\nグリーン車がついております";
+  }else{
+    atos_sentence[9] = "";
+  }
+});
+
+  // 手動追加
+  // あらかじめ手動で入れたい音源
+  $("#ply_self").change(function(){
+    atos_sentence[16] = "空白0.5秒\n" + $("#ply_self").val();
+  })
+
+
+// 増結・切離し
+function add_tr(){
+  // 増結選択時
+  if($("#add_tr_a").val() === "0"){
+    if($("#stop_sta").val() === "92_1"){
+      atos_sentence[10] = "空白0.5秒\n当駅で";
+      atos_sentence[11] = "";
+    }else{
+      atos_sentence[10] = "空白0.5秒\n" + buund_for[$("#stop_sta").val()][1];
+      atos_sentence[11] = "で";
+    }
+    atos_sentence[12] = atos_cross[$("#add_tr_b").val()][1];
+    atos_sentence[13] = cars[$("#add_tr_c").val()][1];
+    atos_sentence[14] = "増結をいたします";
+    atos_sentence[15] = "";
+
+    // 切離し
+  }else if($("#add_tr_a").val() === "1"){
+    atos_sentence[10] = "空白0.5秒\nこの"+res_den+"の";
+    atos_sentence[11] = atos_cross[$("#add_tr_b").val()][1];
+    atos_sentence[12] = cars[$("#add_tr_c").val()][1];
+    atos_sentence[13] = "は";
+
+    if($("#stop_sta").val() === "92_1"){
+      atos_sentence[14] = "当駅";
+    }else{
+      atos_sentence[14] = buund_for[$("#stop_sta").val()][1];
+    }
+    atos_sentence[15] = "止まりとなります";
+    
+      // なし
+  }else if($("#add_tr_a").val() === "2"){
+    for(d=0;d<6;d++){
+      atos_sentence[10+d] = "";
+    }
+
+  }
+}
+// 114*184
+
+// 接近用パーツ順序表示
+function arriv(){
+  list_link = [];
+  playlist = [];
+  $("#playlist_out").val("");
+  // 番線代入
+  if(atos_sentence[0] != ""){
+    jun[2] = atos_sentence[0] + "に";
+    arriv_back[2] = atos_sentence[0] + "に";
+  }else{
+    jun[2] = "";
+    arriv_back[2] = "";
+  }
+  
+
+  // ループで路線愛称から行き先までの音源を自動追加
+  for(d=0;d<5;d++){
+    jun[4+d] = atos_sentence[1+d];
+    arriv_back[12+d] = atos_sentence[1+d];
+  }
+
+  // ドア数&両数
+  if($("#door_car").prop('checked') == true){
+    jun[13] = "空白0.5秒\nこの"+res_den+"は";
+    jun[14] = atos_sentence[7];
+    jun[15] = atos_sentence[8];
+    // jun[16] = "です";
+  }else{
+    for(d=0;d<4;d++){
+      jun[13+d] = "";
+    }
+  }
+  // グリーン追加
+  jun[17] = atos_sentence[9];
+  arriv_back[10] = atos_sentence[9];
+
+  // 増解結案内
+  add_tr();
+  for(d=0;d<6;d++){
+    jun[18+d] = atos_sentence[10+d];
+  }
+
+    // 手動追加
+  // あらかじめ手動で入れたい音源
+  arriv_back[22] = atos_sentence[16];
+  jun[24] = atos_sentence[16];
+
+
+  // 行き先
+  // 2つ(行き分け)
+  if($("[name=bound_set][value=2]").prop('checked')){
+    jun[8] = buund_for[bound][1];
+    jun[9] = buund_for[bound_2][1];
+    jun[10] = "行きが";
+
+    arriv_back[16] = buund_for[bound][1];
+    arriv_back[17] = buund_for[bound_2][1];
+    arriv_back[18] = "";
+  // 単独(非連続)
+  }else if($("[name=bound_set][value=1]").prop('checked')){
+    jun[8] = buund_for[bound][1];
+    jun[9] = "";
+    jun[10] = "行きが";
+    
+    arriv_back[16] = buund_for[bound][1];
+    arriv_back[17] = "";
+    arriv_back[18] = "";
+    // 山手用
+  }else if($('[name=bound_set][value=3]').prop('checked')){
+    jun[8] = buund_for[bound][1];
+
+    for(d=0;d<bound_ga.length;d++){
+      if(buund_for[bound_2][1] + "方面行きが" === bound_ga[d][1]){
+        jun[9] = bound_ga[d][1];
+        jun[10] = "";
+        break;
+      }else if(d === (bound_ga.length-1) && buund_for[bound_2][1] + "方面行きが" != bound_ga[d][1]){
+        jun[9] = buund_for[bound_2][1];
+        jun[10] = "方面行きが";
+      }
+    }
+
+    arriv_back[16] = buund_for[bound][1];
+    arriv_back[17] = buund_for[bound_2][1];
+    arriv_back[18] = "方面";    
+    // 単独(連続)
+  }else if($('[name=bound_set][value=0]').prop('checked')){
+    jun[8] = buund_for[bound][1];
+    jun[9] = "";
+    for(d=0;d<bound_ga.length;d++){
+      if(buund_for[bound][1] + "行きが" === bound_ga[d][1]){
+        jun[8] = bound_ga[d][1];
+        jun[10] = "";
+        break;
+      }else if(d === (bound_ga.length-1) && buund_for[bound][1] + "行きが" != bound_ga[d][1]){
+        jun[8] = buund_for[bound][1];
+        jun[10] = "行きが";
+      }
+    }
+
+    arriv_back[16] = buund_for[bound][1];
+    arriv_back[17] = "";
+    arriv_back[18] = "";
+  }
+
+  // 通常
+  if($('[name=type_2][value=0]').prop('checked')){
+    jun[3] = "";
+    // jun[4] = atos_sentence[1];
+    $("#playlist_out").val(jun.join('\n'));
+
+    // 当駅始発
+  }else if($('[name=type_2][value=1]').prop('checked')){
+    jun[3] = "当駅始発";
+    $("#playlist_out").val(jun.join('\n'));
+
+    // 折り返し
+  }else if($('[name=type_2][value=2]').prop('checked')){
+    // arriv_back[8] = atos_sentence[1]
+    arriv_back[4] = res_den+"がまいります";
+    arriv_back[6] = "この"+res_den+"は";
+
+    // ドア
+    if($("#door_car").prop('checked') == true){
+      arriv_back[7] = atos_sentence[7];
+      arriv_back[8] = atos_sentence[8];
+      // arriv_back[9] = "です";
+      arriv_back[11] = "空白0.5秒\n折り返し";
+    }else{
+      for(d=0;d<3;d++){
+        arriv_back[7+d] = "";
+      }
+      arriv_back[11] = "折り返し";
+    }
+    arriv_back[16] = buund_for[bound][1];
+    arriv_back[21] =　"";
+    $("#playlist_out").val(arriv_back.join('\n'));
+// 清掃ありの折り返し
+  }else if($('[name=type_2][value=6]').prop('checked')){
+    arriv_back[4] = res_den+"がまいります";
+    arriv_back[6] = "この"+res_den+"は";
+
+    // ドア
+    if($("#door_car").prop('checked') == true){
+      arriv_back[7] = atos_sentence[7];
+      arriv_back[8] = atos_sentence[8];
+      // arriv_back[9] = "です";
+      arriv_back[11] = "空白0.5秒\n折り返し";
+    }else{
+      for(d=0;d<3;d++){
+        arriv_back[7+d] = "";
+      }
+      arriv_back[11] = "折り返し";
+    }
+    arriv_back[16] = buund_for[bound][1];
+    arriv_back[21] = "空白0.5秒\n空白0.5秒\nなお\n折り返し\n車内清掃を行います\nすぐの\nご乗車になれませんので\n乗車口で\nお待ちください";
+    $("#playlist_out").val(arriv_back.join('\n'));
+
+    // 簡易放送
+  }else if($('[name=type_2][value=3]').prop('checked')){
+    if(atos_sentence[0] != ""){
+      jun_yuru[2] = atos_sentence[0] + "に";
+    }else{
+      jun_yuru[2] = "";
+    }
+    jun_yuru[3] = "";
+    jun_yuru[4] = "";
+    jun_yuru[5] =  res_den+"がまいります";
+    jun_yuru[7] = "";
+    $("#playlist_out").val(jun_yuru.join('\n'));
+
+    // 種別のみ接近
+  }else if($('[name=type_2][value=4]').prop('checked')){
+    // 種別
+    if(atos_sentence[0] != ""){
+      jun_yuru[2] = atos_sentence[0] + "に";
+    }else{
+      jun_yuru[2] = "";
+    }
+    
+    jun_yuru[5] = "まいります";
+    if(atos_sentence[4] == "回送"){
+      jun_yuru[3] = atos_sentence[4] + res_den +"が";
+      jun_yuru[4] = "";
+      jun_yuru[5] = "まいります";
+      jun_yuru[7] = "この"+res_den+"には\nご乗車になれませんので\nご注意ください";
+    }else if(atos_sentence[4] == ""){
+      jun_yuru[3] = "";
+      jun_yuru[4] = "";
+      jun_yuru[5] = res_den + "がまいります";
+      jun_yuru[7] = "";
+    }else{
+      jun_yuru[3] = atos_sentence[4]
+      jun_yuru[4] = res_den + "が";
+      jun_yuru[5] = "まいります";
+      jun_yuru[7] = "";
+    }
+    $("#playlist_out").val(jun_yuru.join('\n'));
+
+    // 当駅止まり
+  }else if($('[name=type_2][value=7]').prop('checked')){
+    if(atos_sentence[0] != ""){
+      jun_yuru[2] = atos_sentence[0] + "に";
+    }else{
+      jun_yuru[2] = "";
+    }
+    jun_yuru[3] = "当駅止まりの";
+    jun_yuru[4] = "";
+    jun_yuru[5] = res_den+"がまいります";
+    jun_yuru[7] = "この"+res_den+"には\nご乗車になれませんので\nご注意ください"
+
+     $("#playlist_out").val(jun_yuru.join('\n'));
+    // 通過放送
+  }else if($('[name=type_2][value=5]').prop('checked')){
+    if(atos_sentence[0] != ""){
+      jun_yuru[2] = atos_sentence[0] + "を";
+    }else{
+      jun_yuru[2] = "";
+    }
+    
+    jun_yuru[3] = res_den+"が";
+    jun_yuru[4] = "";
+    jun_yuru[5] = "通過いたします";
+    jun_yuru[7] = "";
+    $("#playlist_out").val(jun_yuru.join('\n'));
+
+  }
+  atos_mode = 2;
+  console.log("atos_mode: " + atos_mode);
+}
+
+// 予告用パーツ順序表示
+function yokoku(){
+  list_link = [];
+  playlist = [];
+  $("#playlist_out").val("");
+
+  if($('[name=mor_honj][value=0]').prop('checked')){
+    info[0] = "おはようございます\n空白0.5秒";
+  }else{
+    info[0] = "本日も、ご利用いただき、ありがとうございます\n空白0.5秒";
+  }
+  info[4] = res_den + "は"
+  // 到着前の予告放送
+  if($('[name=type_3][value=0]').prop('checked')){
+    info[1] = "今度の";
+    if(atos_sentence[0] != ""){
+      info[2] = atos_sentence[0] + "の";
+    }else{
+      info[2] = "";
+    }
+    info[3] = "";
+
+    // グリーン
+    info[18] = atos_sentence[9];
+
+    //増解結放送追加
+    add_tr();
+    for(d=0;d<6;d++){
+      info[19+d] = atos_sentence[10+d];
+    }
+
+    // ドア案内
+    if($("#door_car").prop('checked') == true){
+      info[13] = "空白0.5秒\nこの"+res_den+"は";
+      // ドア&両数
+      info[14] = atos_sentence[7];
+      info[15] = atos_sentence[8];
+      // info[16] = "です";
+    }else{
+      // 省略
+      for(d=0;d<4;d++){
+        info[d+13] = "";
+      }
+    }
+    info[26] = "";
+
+    // 出発予告
+  }else if($('[name=type_3][value=1]').prop('checked')){
+    info[1] = "";
+    if(atos_sentence[0] != ""){
+      info[2] = atos_sentence[0] + "に";
+    }else{
+      info[2] = ""
+    }
+    info[3] = "停車中の";
+    // 両数案内省略 & 発車待ち案内
+    info[26] = "空白0.5秒\n発車まで\nしばらくお待ちください";
+    for(d=0;d<6;d++){
+      info[13+d] = "";
+    }
+
+    // 増解結等
+    add_tr();
+    if($("#stop_sta").val() != "93_1"){
+      for(d=0;d<6;d++){
+        info[19+d] = atos_sentence[10+d];
+      }
+    }else{
+      for(d=0;d<6;d++){
+        info[19+d] = "";
+      }
+    }
+  }
+  info[25] = atos_sentence[16];
+
+  // ループで路線愛称から行き先までの音源を自動追加
+  for(d=0;d<6;d++){
+    info[5+d] = atos_sentence[1+d];
+  }
+
+
+
+  // 行き先info[9], info[10]
+  // 2つ(行き分け)
+  if($("[name=bound_set][value=2]").prop('checked')){
+    info[9] = buund_for[bound][1];
+    info[10] = buund_for[bound_2][1];
+    info[11] = "";
+    // 山手用
+  }else if($('[name=bound_set][value=3]').prop('checked')){
+    info[9] = buund_for[bound][1];
+    info[10] = buund_for[bound_2][1];
+    info[11] = "方面";
+    // 単独
+  }else if($('[name=bound_set][value=0]').prop('checked') || $('[name=bound_set][value=1]').prop('checked')){
+    info[9] = buund_for[bound][1];
+    info[10] = "";
+    info[11] = "";
+  }
+
+  // 最終的にセットした予告を、再生リストへ追加
+  $("#playlist_out").val(info.join('\n'));
+  // 予告放送用モード
+  atos_mode = 1;
+  console.log("atos_mode: " + atos_mode);
+}
+
+$('#arriv').on('click', function(){
+  arriv();
+});
+$("#info").on('click', function(){
+  yokoku();
+})
---
+++ melo.js
@@ -0,0 +1,391 @@
+'use strict';
+
+// 初期定義
+let melo = new Audio("");
+let melo_noiz = new Audio("./noiz_melo.mp3");
+melo_noiz.volume = 0.13;
+
+// メロディ(セレクトタブからの選曲時の処理)
+$("#melo_select").change(function(){
+  melo.src = $("#melo_select").val();
+  console.log("now melody: " + melo.src);
+});
+
+// メロディ&ドア閉処理(手動入力時の処理)
+$('#custam_sw').on('click', function(){
+  // 手動入力のところが未入力の時の処理
+  if ($("#melo_input").val() != "") {
+    // セレクトタブで選択されているものを入れる
+    melo.src = $("#melo_input").val();
+  }else{
+    // 手動入力されたものを入れる
+    melo.src = $("#melo_select").val();
+  }
+  // リンク切り替えのデバッグ処理
+  // console.log("now melody: " + melo.src);
+});
+
+// リンク部削除
+$("#melo_url_del").on('click', function(){
+  $("#melo_input").val("");
+})
+
+melo.volume = 0.5; //ボリューム初期設定値
+// door.volume = 0.5; //ボリューム初期設定値
+//ボリューム数値,元値を100倍して出力
+$("#mv_value").html("メロディ用ボリューム" + " 現在:" + Math.floor(melo.volume * 100));
+// ボリューム数値,元値を100倍して出力
+$("#smoking_value").html("禁煙音源ボリューム" + " 現在:" + Math.floor(atos_volume * 100));
+
+// off操作時の禁煙放送停止して、ドア閉め放送再生する処理の関数を定義。
+function off_1(){
+  // off操作時の禁煙放送停止
+  // ドア閉放送種別変更
+    // ○番線
+
+    // /*
+  if($('[name=atos_door][value=0]').prop('checked')){
+    close_atos[0] = atos_sentence[0];
+    for(d=0;d<9;d++){
+      close_atos[1+d] = "";
+    }
+    close_atos[11] = "ドアが閉まります";
+  // ○番線の
+  }else if($('[name=atos_door][value=1]').prop('checked')){
+    if(atos_sentence[0] != ""){
+      close_atos[0] = atos_sentence[0] + "の";
+    }else{
+      close_atos[0] = ""
+    }
+    for(d=0;d<9;d++){
+      close_atos[1+d] = "";
+    }
+    close_atos[10] = ""
+    close_atos[11] = "ドアが閉まります";
+    // ○番線 路線愛称 路線名
+  }else if($('[name=atos_door][value=2]').prop('checked')){
+    if(atos_sentence[0] != ""){
+      close_atos[0] = atos_sentence[0] + "の";
+    }else{
+      close_atos[0] = ""
+    }
+    close_atos[1] = atos_sentence[1];
+
+    if(atos_sentence[5] == "上野"){
+      close_atos[2] = train_line[line_num][1] + "\n上野止まり";
+    }else{
+      close_atos[2] = train_line[line_num][1];
+    }
+    
+    for(d=0;d<8;d++){
+      close_atos[3+d] = "";
+    }
+    close_atos[11] = "ドアが閉まります";
+
+    // ○番線から "種別" が 発車いたします
+  }else if($('[name=atos_door][value=3]').prop('checked')){
+    if(atos_sentence[0] != ""){
+      close_atos[0] = atos_sentence[0] + "から";
+    }else{
+      close_atos[0] = ""
+    }
+
+    for (let d = 0; d < 3; d++) {
+      close_atos[1+d] = "";
+    }
+    if(atos_sentence[4] === "回送" || atos_sentence[4] === "臨時" || atos_sentence[4] === "団体"){
+      close_atos[4] = train_type[kind_num][1] + res_den + "が";
+    }else{
+      close_atos[4] = res_den + "が";
+    }
+    for (let d = 0; d < 4; d++) {
+      close_atos[5+d] = "";
+    }
+    close_atos[9] = "発車いたします";
+    close_atos[11] = "";
+
+    // 仙台式
+  }else if($('[name=atos_door][value=4]').prop('checked')){
+    // 番線設定有無
+    if(atos_sentence[0] != ""){
+      close_atos[0] = atos_sentence[0] + "から";
+    }else{
+      close_atos[0] = "";
+    } 
+    // 
+    for(d=0;d<4;d++){
+      close_atos[1+d] = atos_sentence[1+d];
+    }
+    close_atos[2] = train_line[line_num][1];
+    
+    // 2つ(行き分け)
+    if($("[name=bound_set][value=2]").prop('checked')){
+      close_atos[5] = buund_for[bound][1];
+      close_atos[6] = buund_for[bound_2][1];
+      close_atos[7] = "";
+      close_atos[8] = "行きが";
+      
+      // 山手用
+    }else if($('[name=bound_set][value=3]').prop('checked')){
+      close_atos[5] = buund_for[bound][1];
+      
+      for(d=0;d<bound_ga.length;d++){
+        if(buund_for[bound_2][1] + "方面行きが" === bound_ga[d][1]){
+          close_atos[6] = bound_ga[d][1];
+          close_atos[7] = "";
+          break;
+        }else if(d === (bound_ga.length-1) && buund_for[bound_2][1] + "方面行きが" != bound_ga[d][1]){
+          close_atos[6] = buund_for[bound_2][1];
+          close_atos[7] = "方面行きが";
+        }
+      }
+      
+
+      // 単独(連続パーツ)
+    }else if($('[name=bound_set][value=0]').prop('checked')){
+
+      for(d=0;d<bound_ga.length;d++){
+        if(buund_for[bound][1] + "行きが" === bound_ga[d][1]){
+          close_atos[5] = bound_ga[d][1];
+          close_atos[8] = "";
+          break;
+        }else if(d === (bound_ga.length-1) && buund_for[bound][1] + "行きが" != bound_ga[d][1]){
+          close_atos[5] = buund_for[bound][1];
+          close_atos[8] = "行きが";
+        }
+      }
+      close_atos[6] = "";
+      close_atos[7] = "";
+      // 単独パーツ
+    }else if($('[name=bound_set][value=1]').prop('checked')){
+      close_atos[5] = buund_for[bound][1];
+      close_atos[6] = "";
+      close_atos[7] = "";
+      close_atos[8] = "行きが";
+    }
+
+    close_atos[9] = "発車いたします";
+    // close_atos[10] = "空白0.5秒";
+    close_atos[11] = "空白0.5秒\nドアが閉まります";
+
+  }
+  // 戸閉ATOS再生処理
+  if (atos_mode != 2) {
+    atos1.pause();
+    atos2.pause();
+    list_link = [];
+    playlist = [];
+    $("#playlist_out").val("");
+    if($("#kkkm").prop('checked')){
+      $("#playlist_out").val(close_atos.join('\n') + "\n空白0.5秒\n空白0.5秒\n空白0.5秒\n空白0.5秒\n駆け込み乗車は\nおやめください\n空白0.5秒\n空白0.5秒\n無理なご乗車は\nおやめください\n空白0.5秒\n空白0.5秒\n駆け込み乗車は\nおやめください\n空白0.5秒\n空白0.5秒\n無理なご乗車は\nおやめください");
+    }else{
+      $("#playlist_out").val(close_atos.join('\n'));
+    }
+    soundnum = 0;
+    check_sound();
+    atos_mode = 3;
+  }
+  // */
+
+  // 一時的でバック用
+  /*
+  list_link = [];
+    playlist = [];
+ $("#playlist_out").val("");
+ $("#playlist_out").val("1番線\nドアが閉まります\nご注意ください");
+ soundnum = 0;
+ check_sound();*/
+  
+}
+
+
+// on操作時の処理を関数定義
+function on(){
+  $(function(){
+    
+    melo.play();
+    melo.loop = true;
+    console.log("melody's loop is " + melo.loop);
+    if($("#noiz_check").prop('checked')){
+      melo_noiz.play()
+      melo_noiz.loop = true;
+    }
+  })
+}
+
+//off関数を新たに定義しました。しとかないとoffクリックしたり,dキークリックした時の処理文が長くなってめんどくなる
+function off(){
+    //以下は通常モードの処理
+    if($('[name=sw_mode][value=0]').prop('checked')){
+    melo.pause();
+    melo.currentTime = 0;
+    melo.loop = false;
+    melo_noiz.pause();
+    melo_noiz.currentTime = 0;
+    melo_noiz.loop = false;
+    setTimeout(off_1, 1780);
+
+    console.log("melody's loop is " + melo.loop);
+    }
+    //以下は立川モードの処理
+    else if($('[name=sw_mode][value=1]').prop('checked')){
+      melo.loop = false;
+      setTimeout(off_1, 1780);
+      // door.play();
+      console.log("melody's loop is " + melo.loop);
+    }
+    // 別モード
+    else if($('[name=sw_mode][value=2]').prop('checked')){
+      melo.loop = false;
+      console.log("melody's loop is " + melo.loop);
+    }
+  // $("#on").removeClass().addClass("btn btn-danger btn-lg  text-center");
+}
+
+// メロディが流れ終わってからの処理
+$(melo).on("ended", function(){
+    melo_noiz.pause();
+    melo_noiz.currentTime = 0;
+    melo_noiz.loop = false;
+  if ($('[name=sw_mode][value=2]').prop('checked')) {
+    setTimeout(off_1, 1500);
+  }
+  // 仙台式選択時に、メロディ終了後に駆け込み防止放送再生
+  // if($('[name=atos_door][value=4]').prop('checked')　&& over === true){
+  //   list_link = [];
+  //   playlist = [];
+  //   $("#playlist_out").val("");
+  //   $("#playlist_out").val("駆け込み乗車は\nおやめください\n空白0.5秒\n空白0.5秒\n無理なご乗車は\nおやめください\n空白0.5秒\n空白0.5秒\n駆け込み乗車は\nおやめください\n空白0.5秒\n空白0.5秒\n無理なご乗車は\nおやめください");
+  //   soundnum = 0;
+  //   check_sound();
+  // }
+});
+
+//戸閉放送流れてる時にonを押したら止める処理の関数定義
+function on_door(){
+    if($('[name=on_mode][value=0]').prop('checked')){
+      return;
+    };
+    if($('[name=on_mode][value=1]').prop('checked')){
+      // 禁煙放送強制停止
+      stop_atos();
+    }
+}
+
+// メロディの再生時間と、合計時間表示
+function time(){
+  $(function (){
+    setInterval(function(){
+      let m = ('0' + Math.floor( melo.currentTime / 60 )) .slice( -2 );
+      let s = ( '0' + Math.floor( melo.currentTime % 60 )) .slice( -2 );
+      let dm = ( '0' + Math.floor( melo.duration / 60 ) ) .slice( -2 );
+      let ds = ( '0' + Math.floor( melo.duration % 60 ) ) .slice( -2 );
+      $("#time").html(m + ":" + s + " / " + dm + ":" + ds);
+
+    }, 100);
+  });
+}
+
+time();
+
+function smoking(){
+  list_link = [];
+  playlist = [];
+  $("#playlist_out").val("禁煙放送\n空白0.25秒");
+  soundnum = 0;
+  check_sound()
+}
+
+
+
+$('#smoking').on('click', function(f){
+  smoking();
+});
+$('#on').on('click', function(f) {
+  //on関数召喚
+  on();
+  on_door()
+});
+$('#off').on('click', function (){
+  //off関数召喚
+    off();
+});
+
+// キーボード各種操作
+$('body').on('keydown', function(m){
+  // off操作時
+  if(m.keyCode === 68){
+    //68 = dキー
+    off();
+    // on操作
+  }else if(m.keyCode === 69) {
+    //69キー=Eキー
+    on();
+    on_door()
+    // 禁煙放送操作時
+  }else if(m.keyCode === 88){
+    // 88 = xキー
+    smoking();
+    // ATOS起動
+  }else if(m.keyCode === 71){
+    // 71 = Gキー
+    soundnum = 0;
+    check_sound();
+    // ATOS停止
+  }else if(m.keyCode === 83){
+    // 83 = sキー
+    stop_atos();
+    // 接近
+  }else if(m.keyCode === 82){
+    // 82 = Rキー
+    arriv();
+    // 予告
+  }else if(m.keyCode === 87){
+    // W = 87
+    yokoku();
+  }
+})
+
+// ボタン押した時
+$("#first_off").mousedown(function(){
+  on();
+});
+
+// ボタン離した瞬間　
+$("#first_off").mouseup(function(){
+  melo.loop = false;
+  setTimeout(() => {
+    off_1();
+  }, 10000);
+})
+
+
+//メロディ音源ボリューム制御
+let volume = $("#melo_volume");
+$(volume).change(function() {
+
+  let volumeValue = (volume.val().length == 1) ? '0.0' + volume.val() : '0.' + volume.val();
+
+    if (volumeValue === "0.100") {
+        melo.volume = 1;
+        $("#mv_value").html("メロディ用ボリューム" + " 現在:" + 100);//ボリューム数値,元値を100倍して出力
+    }else{
+      melo.volume = volumeValue;
+      $("#mv_value").html("メロディ用ボリューム" + " 現在:" + (volumeValue * 1000)/ 10);//ボリューム数値,元値を100倍して出力
+    }
+
+  // $(volume).val(volumeValue);
+});
+// 禁煙放送ボリューム制御
+let volume_smoking = $("#smoking_volume");
+$(volume_smoking).change(function(){
+  let volumeValue = (volume_smoking.val().length === 1) ? '0.0' + volume_smoking.val() : '0.' + volume_smoking.val();
+  if(volumeValue === "0.100"){
+    atos_volume = 1;
+    $("#smoking_value").html("禁煙音源ボリューム" + " 現在:" + 100);
+  }else{
+    $("#smoking_value").html("禁煙音源ボリューム" + " 現在:" + ((volumeValue) * 1000)/10);
+    //ボリューム数値,元値を100倍して出力
+    atos_volume = volumeValue;
+  }
+});
